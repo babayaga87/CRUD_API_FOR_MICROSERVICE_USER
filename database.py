1573944cefe4478315b6e89c946de928650fc126
KhangCPU <146229554+babayaga87@users.noreply.github.com>
2025-10-24 21:23:28 +0700

    SQLlite to PostgreSQL

--- database.py
+++ database.py
@@ -3,21 +3,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# BÌNH LUẬN LẠI DÒNG POSTGRESQL GÂY LỖI:
-# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost:5432/user_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost:5432/user_db"
 
-# SỬ DỤNG DÒNG SQLITE NÀY THAY THẾ:
-SQLALCHEMY_DATABASE_URL = "sqlite:///./user.db"  # Sẽ tạo file user.db
+engine = create_engine(SQLALCHEMY_DATABASE_URL) 
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, 
-    connect_args={"check_same_thread": False} # Bắt buộc cho SQLite
-)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency Injection
 def get_db():
     db = SessionLocal()
     try:
