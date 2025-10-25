aeabecb92d461707d602417126ff89d385c37130
KhangCPU <146229554+babayaga87@users.noreply.github.com>
2025-10-25 22:59:53 +0700

    Fix import statement for declarative_base

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 # database.py
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@localhost:5432/user_db"
